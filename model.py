461c364eba0ce765ff3ecd7d0668b5133c54f6af
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2025-11-18 16:29:44 +0300

    translate

--- model.py
+++ model.py
@@ -1,36 +1,37 @@
 import torch
 from transformers import pipeline#, ViTFeatureExtractor, ViTForImageClassification, ViTImageProcessor, FSMTForConditionalGeneration, FSMTTokenizer
 
-pipeline2 = pipeline(
-    task="text2text-generation",
-    model="google-t5/t5-base",
-    dtype=torch.float16,
-    device=0
-);
-
-pipeline3 = pipeline(
-    task="image-classification",
-    model="google/vit-base-patch16-224",
-    dtype=torch.float16,
-    device=0
-)
-result3 = pipeline3("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/pipeline-cat-chonk.jpeg");
-print("Image to text: ", result3);
-# [{'label': 'lynx, catamount', 'score': 0.43460196256637573}, {'label': 'cougar, puma, catamount, mountain lion, painter, panther, Felis concolor', 'score': 0.03484790772199631}, {'label': 'snow leopard, ounce, Panthera uncia', 'score': 0.032355185598134995}, {'label': 'Egyptian cat', 'score': 0.023950593546032906}, {'label': 'tabby, tabby cat', 'score': 0.02285381592810154}]
-detected = result3[0]["label"];
-print("Detected: ", detected);
-
-result2 = pipeline2(f"translate English to Russian: {detected}.");
-print("Translate EN to FR: ", result2);
-
-# pipeline4 = pipeline(
-#     task="text-generation",
-#     model="facebook/wmt19-en-ru",
+# pipeline2 = pipeline(
+#     task="text2text-generation",
+#     model="google-t5/t5-base",
 #     dtype=torch.float16,
 #     device=0
 # );
-# result4 = pipeline4(f"translate English to Russina: {detected}");
-# print("Translate EN to RU: ", result4);
+
+# pipeline3 = pipeline(
+#     task="image-classification",
+#     model="google/vit-base-patch16-224",
+#     dtype=torch.float16,
+#     device=0
+# )
+# result3 = pipeline3("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/pipeline-cat-chonk.jpeg");
+# print("Image to text: ", result3);
+# # [{'label': 'lynx, catamount', 'score': 0.43460196256637573}, {'label': 'cougar, puma, catamount, mountain lion, painter, panther, Felis concolor', 'score': 0.03484790772199631}, {'label': 'snow leopard, ounce, Panthera uncia', 'score': 0.032355185598134995}, {'label': 'Egyptian cat', 'score': 0.023950593546032906}, {'label': 'tabby, tabby cat', 'score': 0.02285381592810154}]
+# detected = result3[0]["label"];
+# print("Detected: ", detected);
+
+# result2 = pipeline2(f"translate English to Russian: {detected}.");
+# print("Translate EN to FR: ", result2);
+
+detected = "Luchs, Katamount.";
+pipeline4 = pipeline(
+    task="text-generation",
+    model="facebook/wmt19-en-ru",
+    dtype=torch.float16,
+    device=0
+);
+result4 = pipeline4(f"translate English to Russina: {detected}");
+print("Translate EN to RU: ", result4);
 
 # from transformers import ViTFeatureExtractor, ViTForImageClassification, ViTImageProcessor
 # from PIL import Image
