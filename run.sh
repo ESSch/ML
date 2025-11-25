309fc96c8fb906cfc192557b3e6f0a63bc78cc60
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2025-11-25 22:23:34 +0300

    optimazed

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 git pull origin main
+
 sudo docker build -t model -f dockerfile_model .
-sudo docker run --name tf --rm -it model python3 model.py
+sudo docker run --name model --rm -it model python3 model.py
+
+sudo docker build -t db -f dockerfile_db .
+sudo docker run -p 8000:8000 db
